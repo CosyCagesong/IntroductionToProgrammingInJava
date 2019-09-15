d435be4df33fc728f40e042ec1661f2f767118ce
Cosy Cagesong <a10063368@gmail.com>
2019-09-15 11:24:59 +0800

    create the repo

---
+++ 1.5InputAndOutput/Sample.java
@@ -0,0 +1,9 @@
+public class Sample{
+	public static void main(String args[]){
+		int n_awefwefwe = Integer.parseInt(args[0]);
+		for(int i=0; i<n_awefwefwe; i++){
+			System.out.println(i);
+		}
+		StdOut.printf("done!");		
+	}
+}
