77d08c33926e40bc1714d4dd79ab2921b02ccbb2
Cosy Cagesong <a10063368@gmail.com>
2019-09-15 13:32:25 +0800

    Newton's method

--- 1.5InputAndOutput/Sample.java
+++ 1.5InputAndOutput/Sample.java
@@ -1,9 +1,12 @@
 public class Sample{
 	public static void main(String args[]){
-		int n_awefwefwe = Integer.parseInt(args[0]);
-		for(int i=0; i<n_awefwefwe; i++){
-			System.out.println(i);
-		}
-		StdOut.printf("done!");		
+		double a=Double.parseDouble(args[0]);
+		double x1=2, diff=100,x2=0;
+		while(diff*diff>=0.000001){
+			x2=x1/2+a/2/x1;
+			diff=x2-x1;
+			x1=x2;
+		StdOut.printf("%.3f",x2
+		);
 	}
 }
