cc20dc517110ebe8a4258f33843fad35879711a5
Cosy Cagesong <a10063368@gmail.com>
2019-09-21 18:44:01 +0800

    changed 2.1

--- 2.1StaticMethods/StaticMethods.java
+++ 2.1StaticMethods/StaticMethods.java
@@ -1,5 +1,15 @@
 public class StaticMethods{
+	public static String duplicate(String s)
+{
+String t = s + s;
+return t;
+}
 	public static void main(String args[]){
+		String s = "Hello";
+s = duplicate(s);
+String t = "Bye";
+t = duplicate(duplicate(duplicate(t)));
+StdOut.println(s + t);
 		
 	}
 	public static int max3(int a, int b, int c){
