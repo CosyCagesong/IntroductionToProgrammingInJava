56c6bfc35cf11615bf1bf315c9dd1e6a1a20fc28
Cosy Cagesong <a10063368@gmail.com>
2019-09-21 16:43:48 +0800

    add 2.1.1

---
+++ 2.1StaticMethods/StaticMethods.java
@@ -0,0 +1,15 @@
+public class StaticMethods{
+	public static void main(String args[]){
+		
+	}
+	public static int max3(int a, int b, int c){
+		if(b>a)a=b;
+		if(c>a)a=c;
+		return a;
+	}
+	public static double max3(double a, double b, double c){
+		if(b>a)a=b;
+		if(c>a)a=c;
+		return a;
+	}
+}
