7ff259eb3f7b3f468c8be54b89fd7b5926d7dfbd
Cosy Cagesong <a10063368@gmail.com>
2019-09-29 08:55:20 +0800

    changed StaticMethods

--- 2.1StaticMethods/StaticMethods.java
+++ 2.1StaticMethods/StaticMethods.java
@@ -1,16 +1,12 @@
 public class StaticMethods{
-	public static String duplicate(String s)
-{
-String t = s + s;
-return t;
-}
+	public static String duplicate(String s){
+		String t = s + s;
+		return t;
+	}
 	public static void main(String args[]){
-		String s = "Hello";
-s = duplicate(s);
-String t = "Bye";
-t = duplicate(duplicate(duplicate(t)));
-StdOut.println(s + t);
-		
+		int N = Integer.parseInt(args[0]);
+		for (int i=0;i<N;i++)
+			System.out.format("%d ",StdRandom.uniform(10));
 	}
 	public static int max3(int a, int b, int c){
 		if(b>a)a=b;
